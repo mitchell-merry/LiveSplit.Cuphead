443ebdd18b144d5862d804c3db1ce1334528e956
Mitchell Merry <mitchellmerry145@gmail.com>
2022-08-27 22:07:08 +1000

    Initial setup

---
+++ Components/CupheadComponent.cs
@@ -0,0 +1,64 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LiveSplit.UI.Components
+{
+    public class CupheadComponent : IComponent
+    {
+        public TimerModel Model { get; set; }
+        public CupheadSettings Settings { get; set; }
+        public string ComponentName => "Cuphead Autosplitter";
+
+        public CupheadComponent(LiveSplitState state)
+        {
+            if (state != null)
+            {
+                Model = new TimerModel() {  CurrentState = state };
+                Model.InitializeGameTime();
+            }
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion) { }
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion) { }
+
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            Settings.Mode = mode;
+            return Settings;
+        }
+
+        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return Settings.GetSettings(document);
+        }
+
+        public void SetSettings(System.Xml.XmlNode settings)
+        {
+            Settings.SetSettings(settings);
+        }
+
+        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
+        public IDictionary<string, Action> ContextMenuControls => null;
+        public float HorizontalWidth => 0;
+        public float MinimumHeight => 0;
+        public float MinimumWidth => 0;
+        public float PaddingBottom => 0;
+        public float PaddingLeft => 0;
+        public float PaddingRight => 0;
+        public float PaddingTop => 0;
+        public float VerticalHeight => 0;
+    }
+}
---
+++ Components/CupheadFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LiveSplit.Model;
+using System.Reflection;
+
+namespace LiveSplit.UI.Components
+{
+    public class CupheadFactory : IComponentFactory
+    {
+        public string ComponentName => "Cuphead Autosplitter v" + Version.ToString();
+        public string Description => "Controls the timer for Cuphead speedruns.";
+        public ComponentCategory Category => ComponentCategory.Control;
+        public IComponent Create(LiveSplitState state) => new CupheadComponent(state);
+        public string UpdateName => ComponentName;
+        public string UpdateURL => "https://raw.githubusercontent.com/mitchell-merry/LiveSplit.Cuphead/main/";
+        public string XMLURL => UpdateURL + "Components/Updates.xml";
+        public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
